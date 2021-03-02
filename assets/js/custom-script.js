09005e573854e148e392ee039891e6c63cfb3032
Emaleth <emaleth@protonmail.com>
2021-03-02 18:56:14 +0100

    new strip

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,19 +1,25 @@
-$(document).ready(function(){
-    var speed = 5;
-    var items, scroller = $('.scroller');
-    var width = 0;
-    scroller.children().each(function(){
-        width += $(this).outerWidth(true);
-    });
-    scroller.css('width', width);
+$(document).ready(function () {
+  var speed = 5;
+  var items,
+    scroller = $(".scroller");
+  var width = 0;
+  scroller.children().each(function () {
+    width += $(this).outerWidth(true);
+  });
+  scroller.css("width", width);
+  scroll();
+  function scroll() {
+    items = scroller.children();
+    var scrollWidth = items.eq(0).outerWidth();
+    scroller.animate(
+      { left: 0 - scrollWidth },
+      (scrollWidth * 100) / speed,
+      "linear",
+      changeFirst
+    );
+  }
+  function changeFirst() {
+    scroller.append(items.eq(0).remove()).css("left", 0);
     scroll();
-    function scroll(){
-        items = scroller.children();
-        var scrollWidth = items.eq(0).outerWidth();
-        scroller.animate({'left' : 0 - scrollWidth}, scrollWidth * 100 / speed, 'linear', changeFirst);
-    }
-    function changeFirst(){
-        scroller.append(items.eq(0).remove()).css('left', 0);
-        scroll();
-    }
+  }
 });
